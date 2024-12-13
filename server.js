872f485e290b6ec90f7307cace886bbfef4a2a43
Bozorgmehr Zia <bozorgmehr1381@gmail.com>
2024-12-13 13:36:28 +0330

    Add question routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors'); // Import CORS
 const setupSwagger = require('./swagger');
 const userRoutes = require('./routes/users');
 const tagRoutes = require('./routes/tags');
+const questionRoutes = require('./routes/questions');
 const app = express();
 const port = process.env.PORT || 9090;
 
@@ -16,6 +17,7 @@ setupSwagger(app);
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/tag', tagRoutes);
+app.use('/api/question', questionRoutes);
 
 // Start the server
 app.listen(port, () => {
